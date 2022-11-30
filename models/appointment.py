fada999aecd3f5750e986b90c0ed2d8d71c30b75
Pau Pericàs <errorandtrialweb@gmail.com>
2022-11-30 20:16:08 +0100

    Signed-off-by: Pau Pericàs <errorandtrialweb@gmail.com>

--- models/appointment.py
+++ models/appointment.py
@@ -2,94 +2,200 @@
 
 from odoo import models, fields, api, _
 from odoo import tools
+from datetime import datetime
+from odoo.exceptions import ValidationError, UserError
 
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
-    
+
     def app_id_auto(self):
         try:
             return self._context.get('active_ids')[0]
         except Exception:
             return False
-    appointment_id = fields.Many2one('veterinary.appointment',default=lambda self: self.app_id_auto()  )
+
+    appointment_id = fields.Many2one('veterinary.appointment', default=lambda self: self.app_id_auto())
+
 
 class Appointment(models.Model):
     _name = "veterinary.appointment"
     _order = "dateOfAppointment desc"
-    name = fields.Char(string='Name',readonly=True,default=lambda self: _('New'))
+    name = fields.Char(string='Name', readonly=True, default=lambda self: _('New'))
     description = fields.Char('Description')
-    partner_id = fields.Many2one('res.partner',string='Owner',required=True)
-    dateOfAppointment = fields.Datetime('Date of Appointment',required=True)
-    animals = fields.Many2many('veterinary.animal',string='Animals')
+    partner_id = fields.Many2one('res.partner', string='Owner', required=True)
+    dateOfAppointment = fields.Datetime('Date of Appointment', required=True)
+    animals = fields.Many2many('veterinary.animal', string='Animals')
     evaluation_id = fields.One2many('veterinary.evaluation', 'name', string='Evaluation')
     telephone = fields.Char(related='partner_id.mobile')
     animal_id = fields.Many2one('veterinary.animal')
-    user_id = fields.Many2one('res.users', string='Doctor',required=True,track_visibility='onchange',default=lambda self: self.env.user)
+    user_id = fields.Many2one('res.users', string='Doctor', required=True, track_visibility='onchange',
+                              default=lambda self: self.env.user)
     cancel_reason = fields.Text('Reason of cancellation')
-    invoice_ids = fields.One2many('account.invoice','appointment_id', string="Appointment Id")
-    total_invoiced = fields.Char('Total',compute='_total_count')
+    invoice_ids = fields.One2many('account.invoice', 'appointment_id', string="Appointment Id")
+    total_invoiced = fields.Char('Total', compute='_total_count')
     state = fields.Selection(
-        [('draft','Draft'),
-        ('confirm','Confirm'),
-        ('done','Done'),
-        ('cancel','Cancel')]
-        , string='Status', index=True, readonly=True, default='draft',
+        [('draft', 'Draft'),
+         ('confirm', 'Confirm'),
+         ('done', 'Done'),
+         ('cancel', 'Cancel')]
+        , string='Status', index=True, default='draft',
         track_visibility='onchange', copy=False
     )
-    
+
     @api.model
     def create(self, vals):
         vals['name'] = self.env['ir.sequence'].next_by_code('appointment.number') or _('New')
         res = super(Appointment, self).create(vals)
         return res
-    
-    @api.multi
-    def action_create_invoice(self):
-        for appointment in self:
-            inv = {
-            'appointment_id': self._name,
-            'partner_id': self.partner_id.id,
-            }
-            invoiced = self.env['account.invoice'].create(inv)
 
-    @api.one
-    def action_create_invoice(self):
-        inv = {
-            'appointment_id': self._name,
-            'partner_id': self.partner_id.id,
-            }
-        invoiced = self.env['account.invoice'].create(inv)
-    
     @api.multi
     def _total_count(self):
-        t = self.env['account.invoice'].search([['appointment_id','=',self.id]])
-        self.total_invoiced  = len(t)
-    
+        t = self.env['account.invoice'].search([['appointment_id', '=', self.id]])
+        self.total_invoiced = len(t)
+
     @api.multi
     def action_cancel_appointment(self):
         return self.write({'state': 'cancel'})
-    
+
     def invoice_view(self):
         action = self.env.ref('account.action_invoice_refund_out_tree')
         result = action.read()[0]
         result['domain'] = [('appointment_id', '=', self.id)]
         return result
-    
+
+    def _prepare_invoice_data(self, cr, uid, context=None):
+        context = context or {}
+
+        journal_obj = self.env['account.journal']
+
+        if not self.partner_id:
+            raise ValidationError(_('No Customer Defined!'),
+                                 _("You must first select a Customer for Contract %s!") % self.name)
+
+        fpos = False
+
+        journal_ids = journal_obj.search([('type', '=', 'sale'),
+                                                   ('company_id', '=', self.user_id.company_id.id or False)], limit=1)
+        if not journal_ids:
+            raise ValidationError(_('Error!'),
+                                 _('Please define a sale journal for the company "%s".') % (
+                                 self.user_id.company_id.name or '',))
+
+        property_payment_term = self.env['account.payment.term']
+        partner_payment_term = property_payment_term.search([], limit=1)
+
+        if self.partner_id.property_product_pricelist:
+            currency_id = self.partner_id.property_product_pricelist.currency_id.id
+        elif self.user_id.company_id:
+            currency_id = self.user_id.company_id.currency_id.id
+
+        invoice = {
+            'account_id': self.partner_id.property_account_payable_id.id,
+            'type': 'out_invoice',
+            'partner_id': self.partner_id.id,
+            'currency_id': currency_id,
+            'journal_id': len(journal_ids) and journal_ids[0] or False,
+            'date_invoice': datetime.today(),
+            'origin': self.name,
+            'fiscal_position': fpos and fpos.id,
+            'payment_term': partner_payment_term,
+            'company_id': self.user_id.company_id.id or False,
+        }
+        return invoice
+
+    def _prepare_invoice_lines(self, cr, uid, fiscal_position_id, context=None):
+        fpos_obj = self.pool.get('account.fiscal.position')
+        fiscal_position = None
+        if fiscal_position_id:
+            fiscal_position = fpos_obj.browse(cr, uid, fiscal_position_id, context=context)
+        invoice_lines = []
+        # for line in contract.recurring_invoice_line_ids:
+        #
+        #     res = line.product_id
+        #     account_id = res.property_account_income.id
+        #     if not account_id:
+        #         account_id = res.categ_id.property_account_income_categ.id
+        #     account_id = fpos_obj.map_account(cr, uid, fiscal_position, account_id)
+        #
+        #     taxes = res.taxes_id or False
+        #     tax_id = fpos_obj.map_tax(cr, uid, fiscal_position, taxes)
+        #
+        #     invoice_lines.append((0, 0, {
+        #         'name': line.name,
+        #         'account_id': account_id,
+        #         'account_analytic_id': contract.id,
+        #         'price_unit': line.price_unit or 0.0,
+        #         'quantity': line.quantity,
+        #         'uos_id': line.uom_id.id or False,
+        #         'product_id': line.product_id.id or False,
+        #         'invoice_line_tax_id': [(6, 0, tax_id)],
+        #     }))
+        return invoice_lines
+
+    def _prepare_invoice(self, cr, uid, context=None):
+        invoice = self._prepare_invoice_data(cr, uid,  context)
+        invoice['invoice_line'] = self._prepare_invoice_lines(cr, uid, invoice['fiscal_position'], context)
+        return invoice
+
     @api.one
     def action_confirm(self):
         self.state = 'confirm'
         for animal in self.animals:
             pick = {
-            'animal': animal.id,
-            'appointment_id': self.id,
-            'partner_id': self.partner_id.id,
+                'animal': animal.id,
+                'appointment_id': self.id,
+                'partner_id': self.partner_id.id,
             }
             picking = self.env['veterinary.evaluation'].create(pick)
         return self.invoice_view()
 
     @api.one
+    def action_create_invoice(self, grouped=False, final=False):
+        inv_obj = self.env['account.invoice']
+        precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+        invoice = self._prepare_invoice(self, self.env.cr, self.env.uid)
+        references = {}
+        invoices_origin = {}
+        invoices_name = {}
+
+        # for appointment in self:
+        #     group_key = appointment.id
+        #     for line in appointment.evaluation_id.prescriptions.sorted(key=lambda l: l.qty_to_invoice < 0):
+        #         if float_is_zero(line.qty_to_invoice, precision_digits=precision):
+        #             continue
+        #         if line.qty_to_invoice > 0:
+        #             line.invoice_line_create(invoices[group_key].id, line.qty_to_invoice)
+        #         elif line.qty_to_invoice < 0 and final:
+        #             line.invoice_line_create(invoices[group_key].id, line.qty_to_invoice)
+        #
+        # for group_key in invoices:
+        #     invoices[group_key].write({'name': ', '.join(invoices_name[group_key]),
+        #                                'origin': ', '.join(invoices_origin[group_key])})
+        #
+        # if not invoices:
+        #     raise UserError(_('There is no invoiceable line.'))
+
+        invoice.compute_taxes()
+        if not invoice.invoice_line_ids:
+            raise UserError(_('There is no invoiceable line.'))
+        # If invoice is negative, do a refund invoice instead
+        if invoice.amount_total < 0:
+            invoice.type = 'out_refund'
+            for line in invoice.invoice_line_ids:
+                line.quantity = -line.quantity
+        # Use additional field helper function (for account extensions)
+        for line in invoice.invoice_line_ids:
+            line._set_additional_fields(invoice)
+        # Necessary to force computation of taxes and cash rounding. In account_invoice, they are triggered
+        # by onchanges, which are not triggered when doing a create.
+        invoice.compute_taxes()
+        invoice._onchange_cash_rounding()
+        invoice.message_post_with_view('mail.message_origin_link',
+                                       values={'self': invoice, 'origin': references[invoice]},
+                                       subtype_id=self.env.ref('mail.mt_note').id)
+        return [invoice.id]
+
+    @api.one
     def action_done(self):
         self.state = 'done'
-        
-    
--- models/evaluation.py
+++ models/evaluation.py
@@ -9,33 +9,36 @@ from odoo.tools import float_is_zero, float_compare, DEFAULT_SERVER_DATETIME_FOR
 from odoo.tools.misc import formatLang
 
 
-
 class Evaluation(models.Model):
     _name = 'veterinary.evaluation'
     _inherit = ['mail.thread']
 
     @api.multi
     def default_stage(self):
-        return self.env['veterinary.evaluation.stages'].search([('name','=','New')])
-    
+        return self.env['veterinary.evaluation.stages'].search([('name', '=', 'New')])
+
     @api.model
-    def _read_group_stage_ids(self,stages,domain,order):
+    def _read_group_stage_ids(self, stages, domain, order):
         stage_ids = self.env['veterinary.evaluation.stages'].search([])
         return stage_ids
 
-    name =fields.Char('Name',compute='_compute_name')
-    animal = fields.Many2one('veterinary.animal', required=True,string='Animal',readonly=True)
-    appointment_id = fields.Many2one('veterinary.appointment',string='Appointment')
-    description  = fields.Char(string="Reason for consultation", store=True, related='appointment_id.description')
+    name = fields.Char('Name', compute='_compute_name')
+    animal = fields.Many2one('veterinary.animal', string='Animal', readonly=True)
+    appointment_id = fields.Many2one('veterinary.appointment', string='Appointment')
+    description = fields.Char(string="Reason for consultation", store=True, related='appointment_id.description')
 
     current_illness = fields.Text('Current illness')
-    stage_id = fields.Many2one('veterinary.evaluation.stages',string='Stage',required=True,default=default_stage,group_expand='_read_group_stage_ids')
-    user_id = fields.Many2one('res.users', string='Doctor', index=True, track_visibility='onchange', default=lambda self: self.env.user)
-    partner_id = fields.Many2one('res.partner', string='Owner',required=True,readonly=True)
-    conditions = fields.Many2many('veterinary.code', 'cod_eval_rel',  'name', 'code', domain="[('category', '=', 'Condition')]")
-    procedures = fields.Many2many('veterinary.code', 'proc_eval_rel',  'name', 'code', domain="[('category', '=', 'Procedure')]")
-    prescriptions = fields.Many2many('product.product', 'prod_eval_rel',  'name', 'product_id')
-    
+    stage_id = fields.Many2one('veterinary.evaluation.stages', string='Stage', required=True, default=default_stage,
+                               group_expand='_read_group_stage_ids')
+    user_id = fields.Many2one('res.users', string='Doctor', index=True, track_visibility='onchange',
+                              default=lambda self: self.env.user)
+    partner_id = fields.Many2one('res.partner', string='Owner', required=True, readonly=True)
+    conditions = fields.Many2many('veterinary.code', 'cod_eval_rel', 'name', 'code',
+                                  domain="[('category', '=', 'Condition')]")
+    procedures = fields.Many2many('veterinary.code', 'proc_eval_rel', 'name', 'code',
+                                  domain="[('category', '=', 'Procedure')]")
+    prescriptions = fields.Many2many('product.product', 'prod_eval_rel', 'name', 'product_id')
+
     # Musculoskeletal System Page
     conformation = fields.Char('Conformation')
     feet_shoeing = fields.Char('Feet and Shoeing')
@@ -49,48 +52,48 @@ class Evaluation(models.Model):
     diagnostic_imaging = fields.Char('Diagnostic Imaging')
     walk = fields.Char('Walk')
     trot = fields.Char('Trot')
-    
-    #Respiratory System
+
+    # Respiratory System
     res_general = fields.Char('General')
     lung_auscultation = fields.Char('Lung auscultation')
     upper_airway_endoscopy = fields.Char('Upper airway endoscopy')
-    
-    #Cardiovascular System
+
+    # Cardiovascular System
     cardi_general = fields.Char('General')
     auscultation = fields.Char('Auscultation')
     ecg = fields.Char('ECG')
-    
-    #Gastrointestinal System
+
+    # Gastrointestinal System
     gest_general = fields.Char('General')
     worming_history = fields.Char('Worming Histroy')
     teeth = fields.Char('Teeth')
 
-    #Nervous System
+    # Nervous System
     nevr_general = fields.Char('General')
     mentation = fields.Char('Mentation')
     gait = fields.Char('Gait')
     eyes = fields.Char('Eyes')
-    
-    #Reproductive and Uniary System
+
+    # Reproductive and Uniary System
     fig = fields.Char('Female / Intact Male / Gelding')
     testicles = fields.Char('Testicles')
     vulva = fields.Char('Vulva')
-    
-    #Skin
+
+    # Skin
     scars = fields.Char('Scars - Traumatic / Surgical')
     melanomas = fields.Char('Tumours - Melanomas Sarcoids')
     Dermatitis = fields.Char('Dermatitis')
     Allergy = fields.Char('Allergy')
     skin_other = fields.Char('Other')
-    
+
     other = fields.Text('Other')
     overall_assessment = fields.Text('Overall Assessment')
     recommendations = fields.Text('Recommendations')
-    
+
     def _compute_name(self):
         self.name = self.appointment_id.name
         return True
-    
+
     @api.one
     def action_create_invoice(self):
         self.appointment_id.action_create_invoice
@@ -110,11 +113,6 @@ class Evaluation(models.Model):
             template_id = ir_model_data.get_object_reference('veterinary', 'email_template_edi_sale')[1]
         except ValueError:
             template_id = False
-        # try:
-        #     compose_form_id = ir_model_data.get_object_reference('mail', 'email_compose_message_wizard_form')[1]
-        #     print 'tempalte mail'
-        # except ValueError:
-        #     compose_form_id = False
         ctx = {
             'default_model': 'veterinary.evaluation',
             'default_res_id': self.ids[0],
@@ -131,22 +129,11 @@ class Evaluation(models.Model):
             'view_type': 'form',
             'view_mode': 'form',
             'res_model': 'mail.compose.message',
-            # 'views': [(compose_form_id, 'form')],
-            # 'view_id': compose_form_id,
             'target': 'new',
             'context': ctx,
         }
 
+
 class EvaluationStages(models.Model):
     _name = 'veterinary.evaluation.stages'
     name = fields.Char('Stage')
-
-# class MailComposeMessage(models.TransientModel):
-#     _inherit = 'mail.compose.message'
-
-#     @api.multi
-#     def send_mail(self, auto_commit=False):
-#         if self._context.get('default_model') == 'veterinary.evaluation' and self._context.get('default_res_id') and self._context.get('mark_so_as_sent'):
-#             order = self.env['veterinary.evaluation'].browse([self._context['default_res_id']])
-#             self = self.with_context(mail_post_autofollow=True)
-#         return super(MailComposeMessage, self).send_mail(auto_commit=auto_commit)
